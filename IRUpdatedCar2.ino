706f675bd62e9c7fff73a074ba2e7c60c38ded45
zaraavoyan <31899165+zaraavoyan@users.noreply.github.com>
2019-04-04 21:09:23 -0700

    Update IRUpdatedCar2.ino

--- IRUpdatedCar2.ino
+++ IRUpdatedCar2.ino
@@ -1,5 +1,6 @@
+// Declaring motor pins, receiver pin, and color pins
+
 int RECV_PIN = 11;
-//motor pins
 int M1a = 10;
 int M1b = 9;
 int M2a = 6;
@@ -9,12 +10,11 @@ int bluePin = 2;
 int whitePin = 4;
 
 
-//servo
+// Servo: Include libraries and set position 
 long int check = 0;
 int servoTime = 500;
 int pos = 0;
 #include <Servo.h>
-
 Servo myservo;
 
 //Values controlling the movement
@@ -27,12 +27,16 @@ float LR = 0;
 int pressed = 0x00;
 int piezoPin = 3;
 
-
+// Include Remote Library
 #include <IRremote.h>
 
 
+// Declare pin of the receiver 
+
 IRrecv irrecv(RECV_PIN);
 decode_results results;
+
+// Check value of the remote
 
 int irCheck(boolean Send) {
   int value = 0x00;
@@ -47,13 +51,11 @@ int irCheck(boolean Send) {
   delay(100);
 }
 
-
-
-//Here I'm just making an array of buttons so we can just call numbers easily when checking things.
-//If you want to add more buttons, jsut add to the initial array value and add the button hex code to the array. Make sure to add the name into the comment next to it so we can refer to it later.
-//also don't forget to change irCheck(false) to true. it will let you see the values you need to add the the array.
+//Making an array of buttons so we can just call numbers easily when checking things.
+//To add more, just add to the initial array value and add the button hex code to the array. Make sure to add the name into the comment next to it so we can refer to it later.
 
 int IRButtons[9] = {0xFDA05F, 0xFDB04F, 0xFD10EF, 0xFD50AF, 0xFD906F, 0xFD08F7, 0xFD8877, 0xFD40BF, 0xFD40BF};
+
 //UP[0], DOWN[1], LEFT[2], RIGHT[3], CENTER[4]
 
 void setup() {
@@ -69,21 +71,17 @@ void setup() {
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(piezoPin, OUTPUT);
-
-
 }
 
 void loop() {
-
-
-
-  //TEMP value for the Reading
-  //irCheck is a function I made that holds the ir reading function and returns an int holding the sent data.
-  //the false in the irCheck function enables the function to print to the Serial monitor. This is useful for adding new button controls.
+  // TEMP value for the Reading
+  // irCheck is a function that holds the IR reading function and returns an int holding the sent data.
+  // The 'false' in the irCheck function enables the function to print to the Serial monitor. This is useful for adding new button controls.
   int currentVal = irCheck(true);
-  //0xFFFFFFFF is the int value given when you hold a button down. So you can continuiously hold down a button and it'll read.
+  // 0xFFFFFFFF is the int value given when you hold a button down. So you can continuiously hold down a button and it'll read.
   if (currentVal == 0xFFFFFFFF) {
     //Serial.println("HELD");
+    //Assigns actions to buttons
     if (pressed == IRButtons[0]) Up();
     if (pressed == IRButtons[1]) Down();
     if (pressed == IRButtons[2]) Left();
@@ -93,21 +91,20 @@ void loop() {
     if (pressed == IRButtons[6]) LightsOff();
     if (pressed == IRButtons[7]) beep();
     if (pressed == IRButtons[8]) wave();
-  } else if (currentVal != 0) {
-    //stores the initial button press.
+   } else if (currentVal != 0) {
+    // stores the initial button press.
     pressed = currentVal;
   }
-  //If statements and ternary operators that handle the resetting to default values.
-  //Serial.println(FB);
+  // If statements and ternary operators that handle the resetting to default values.
+  // Serial.println(FB);
   if (FB != 0) FB = FB < 0 ? FB += .7 : FB -= .7;
   if (LR != 0) LR = LR < 0 ? LR += .7 : LR -= .7;
-
-  //custom Function that handles the motor analog write functions
+  // custom Function that handles the motor analog write functions
   UpdateMotors();
   LightsOnIfNotMoving();
 }
-bool Stopped = true;
 
+// Beeps the piezo buzzer 
 void beep() {
   delayMicroseconds(sin(micros()*PI / 100) * 1000);
   digitalWrite(piezoPin, HIGH);
@@ -116,36 +113,31 @@ void beep() {
 }
 
 void UpdateMotors() {
-  //LEFT MOTOR
+  // LEFT MOTOR
   analogWrite(M1a, constrain(FB  - LR, 0, 255));
   analogWrite(M1b, constrain(-FB + LR, 0, 255));
-  //RIGHT MOTOR
+  // RIGHT MOTOR
   analogWrite(M2a, constrain(FB  + LR, 0, 255));
   analogWrite(M2b, constrain(-FB - LR, 0, 255));
 }
 
-//Functions to handle the button presses.
+// Functions to handle the button presses.
 void Up() {
   setColor(0, 255, 0);
   Serial.println("Up");
   FB = constrain(FB + 50, 0, 255);
-
 }
 
 void Down() {
   setColor(0, 0, 255);
   Serial.println("Down");
   FB = constrain(FB - 50, -255, 0);
-
-
-
 }
 
 void Left() {
   setColor(255, 0, 0);
   Serial.println("Left");
   LR = constrain(LR + 50, 0, 255);
-
 }
 
 void Right() {
@@ -159,19 +151,17 @@ void Stop() {
   Serial.println("Stop");
   FB = 0;
   LR = 0;
-
   LightsOn();
-
-
 }
 
-
+// Give color to lights
 void setColor(int redValue, int greenValue, int blueValue) {
   analogWrite(orangePin, redValue);
   analogWrite(bluePin, greenValue);
   analogWrite(whitePin, blueValue);
 }
 
+// Turn on the 'break' lights if the car is not moving
 void LightsOnIfNotMoving() {
   if (abs(LR) < 1 && abs(FB) < 1) {
     LeftOn();
@@ -190,39 +180,45 @@ void LightsOnIfNotMoving() {
       RightOff();
     }
   }
-
 }
 
+// Turns left light on
 void LeftOn() {
   digitalWrite(13, LOW);
-
 }
+
+// Turns left light off
 void LeftOff() {
   digitalWrite(13, HIGH);
-
 }
 
+// Turns right light on
 void RightOn() {
   digitalWrite(12, LOW);
 }
+
+// Turns right light off
 void RightOff() {
   digitalWrite(12, HIGH);
 }
+
+// Turns lights on
 void LightsOn() {
   digitalWrite(12, LOW);
   digitalWrite(13, LOW);
   Serial.println("LightsOn");
   setColor(255, 0, 0);
-
 }
 
+// Turns lights off
 void LightsOff() {
   digitalWrite(12, HIGH);
   digitalWrite(13, HIGH);
-  Serial.println("LightsOn");
+  Serial.println("LightsOff");
   setColor(0, 0, 0);
 }
 
+// Code to wave the arms (a bit glitchy)
 void wave() {
   if (millis() - check < servoTime) {
     myservo.write(map(millis() - check, 0, servoTime, 0, 60));
